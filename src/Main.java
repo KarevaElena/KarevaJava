76af4c9a2c11e03fc002307a7b2b3f4c8cc583f8
Elena Kareva <averak84@gmail.com>
2023-07-09 00:38:47 +0300

    Три задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        int a = 9;
+        if (a > 7) {
+            System.out.println("Привет");
+        }
+        // Задание 2
+        String name = "Вячеслав";
+        if (name == "Вячеслав") {
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+        // Задание 3
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 3 == 0) {
+                System.out.println(array[i]);
+            }
+        }
     }
 }
